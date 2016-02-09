a14c3f3f80a1eb0ee8b0cfda9be09ec2f6d0c1cf
v-ramachandran <v.ram28@gmail.com>
2016-02-09 00:24:12 -0600

    Fixing spacing from four spaces to two

--- json-serialization-examples/src/main/java/venket/examples/serialization/json/model/Course.java
+++ json-serialization-examples/src/main/java/venket/examples/serialization/json/model/Course.java
@@ -2,47 +2,47 @@ package venket.examples.serialization.json.model;
 
 public class Course {
 
-    public static class Listing {
-        private String departmentId;
-        private String courseId;
+  public static class Listing {
+    private String departmentId;
+    private String courseId;
 
-        public Listing(final String departmentId, final String courseId) {
-            this.departmentId = departmentId;
-            this.courseId = courseId;
-        }
-
-        public String getDepartmentId() {
-            return departmentId;
-        }
-
-        public String getCourseId() {
-            return courseId;
-        }
-
-        @Override
-        public String toString() {
-            return "Listing [departmentId=" + departmentId + ", courseId=" + courseId + "]";
-        }
+    public Listing(final String departmentId, final String courseId) {
+      this.departmentId = departmentId;
+      this.courseId = courseId;
     }
 
-    private String name;
-    private Listing listing;
-
-    public Course(final String name, final Listing listing) {
-        this.name = name;
-        this.listing = listing;
+    public String getDepartmentId() {
+      return departmentId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Listing getListing() {
-        return listing;
+    public String getCourseId() {
+      return courseId;
     }
 
     @Override
     public String toString() {
-        return "Course [name=" + name + ", listing=" + listing + "]";
+        return "Listing [departmentId=" + departmentId + ", courseId=" + courseId + "]";
     }
+  }
+
+  private String name;
+  private Listing listing;
+
+  public Course(final String name, final Listing listing) {
+    this.name = name;
+    this.listing = listing;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Listing getListing() {
+    return listing;
+  }
+
+  @Override
+  public String toString() {
+    return "Course [name=" + name + ", listing=" + listing + "]";
+  }
 }
